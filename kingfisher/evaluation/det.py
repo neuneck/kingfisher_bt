80a2fff8f9598adb82d49a9bc31c8620ff642d96
Julian Schweizer <js@nect.com>
2023-08-10 14:19:20 +0000

    Fixed a bug in det computation when external bins are provided

--- kingfisher/evaluation/det.py
+++ kingfisher/evaluation/det.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def get_fpr_fnr(scores, truth, n_steps: int = 1000, hist_bins=None):
     """Plot a single detection/error tradeoff curve."""
-    hist_bins = hist_bins or get_hist_bins(scores, n_steps)
+    if hist_bins is None:
+        hist_bins = get_hist_bins(scores, n_steps)
     pos_pmf = _get_pmf(scores, truth, True, hist_bins)
     neg_pmf = _get_pmf(scores, truth, False, hist_bins)
 
